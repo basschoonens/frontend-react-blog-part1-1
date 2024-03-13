57db855c169ba67f48ff8cbecd3e0515dbd8480b
basschoonens <bas.schoonens@gmail.com>
2024-03-13 17:33:49 +0100

    blogposts geimporteerd

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
 import './Home.css';
+import posts from '../../constants/data.json';
 
 export default function Home() {
+
+
     return (
         <div className="home">
             <div className="home-container">
--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -1,10 +1,19 @@
 import './Overview.css';
+import posts from "../../constants/data.json";
 
 export default function Overview() {
+
     return (
         <div className="overview">
             <div className="overview-container">
-                <h1>Overview</h1>
+                <h1>Bekijk alle {posts.length} posts op het platform.</h1>
+                <ul className="overview-posts">
+                    {posts.map((post) => (
+                        <li key={post.id}>
+                            <h2>{post.title}</h2>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
