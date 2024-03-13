5d6a5ed032845f65b11e5f86f4637d33adb13c0a
basschoonens <bas.schoonens@gmail.com>
2024-03-13 17:09:56 +0100

    styling verbeterd.

--- src/pages/newPost/NewPost.jsx
+++ src/pages/newPost/NewPost.jsx
@@ -2,8 +2,8 @@ import './NewPost.css';
 
 export default function NewPost() {
     return (
-        <div className="newpost">
-            <div className="newpost-container">
+        <div className="new-post">
+            <div className="new-post-container">
                 <h1>New Post</h1>
             </div>
         </div>
